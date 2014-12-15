c32acbc54ac587880f074301999aa99aec45a149
deividscriollo <deividscriollo@gmail.com>
2014-12-15 16:12:06 -0500

    usuario

---
+++ data/menu/index.php
@@ -0,0 +1,252 @@
+<?php
+//verificacion si esta iniciada la variable se ssesion 
+if(!isset($_SESSION)){
+	session_start();
+}
+//informacion empresa
+function empresa(){
+	print'DOW DELOPERS';
+}
+//Menu banner arriba usuario perfil
+function menu_arriba(){
+	print'
+	<div id="navbar" class="navbar navbar-default">
+			<script type="text/javascript">
+				try{ace.settings.check("navbar" , "fixed")}catch(e){}
+			</script>
+
+			<div class="navbar-container" id="navbar-container">
+				<button type="button" class="navbar-toggle menu-toggler pull-left" id="menu-toggler" data-target="#sidebar">
+					<span class="sr-only">Toggle sidebar</span>
+
+					<span class="icon-bar"></span>
+
+					<span class="icon-bar"></span>
+
+					<span class="icon-bar"></span>
+				</button>
+
+				<div class="navbar-header pull-left">
+					<a href="#" class="navbar-brand">
+						<small>
+							<i class="fa fa-credit-card"></i>
+							';print empresa(); print'
+						</small>
+					</a>
+				</div>
+
+				<div class="navbar-buttons navbar-header pull-right" role="navigation">
+					<ul class="nav ace-nav">
+						<li class="light-blue">
+							<a data-toggle="dropdown" href="#" class="dropdown-toggle">
+								<img class="nav-user-photo" src="../../dist/avatars/user.jpg" alt="Jasons Photo" />
+								<span class="user-info">
+									<small>Bienvenido,</small>
+									Jason
+								</span>
+
+								<i class="ace-icon fa fa-caret-down"></i>
+							</a>
+
+							<ul class="user-menu dropdown-menu-right dropdown-menu dropdown-yellow dropdown-caret dropdown-close">
+								<li>
+									<a href="#">
+										<i class="ace-icon fa fa-cog"></i>
+										Configuración
+									</a>
+								</li>
+
+								<li>
+									<a href="profile.html">
+										<i class="ace-icon fa fa-user"></i>
+										Perfil
+									</a>
+								</li>
+
+								<li class="divider"></li>
+
+								<li>
+									<a href="#">
+										<i class="ace-icon fa fa-power-off"></i>
+										Salir
+									</a>
+								</li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+			</div><!-- /.navbar-container -->
+		</div>'
+	;
+}
+//Menu Latera perfil aplicacion
+function menu_lateral(){
+	print'
+		<div id="sidebar" class="sidebar responsive">
+				<script type="text/javascript">
+					try{ace.settings.check("sidebar" , "fixed")}catch(e){}
+				</script>
+				<ul class="nav nav-list">
+					<li class="active">
+						<a href="../inicio/">
+							<i class="menu-icon fa fa-home"></i>
+							<span class="menu-text"> Inicio </span>
+						</a>
+
+						<b class="arrow"></b>
+					</li>					
+					<li class="">
+						<a href="#" class="dropdown-toggle">
+							<i class="menu-icon fa fa-desktop"></i>
+							<span class="menu-text">
+								Ingresos
+							</span>
+
+							<b class="arrow fa fa-angle-down"></b>
+						</a>
+
+						<b class="arrow"></b>
+
+						<ul class="submenu">
+							<li class="">
+								<a href="#" class="dropdown-toggle">
+									<i class="menu-icon fa fa-caret-right"></i>
+
+									Generales
+									<b class="arrow fa fa-angle-down"></b>
+								</a>
+
+								<b class="arrow"></b>
+
+								<ul class="submenu">
+									<li class="">
+										<a href="../bodegas/">
+											<i class="menu-icon fa fa-caret-right"></i>
+											Bodegas
+										</a>
+
+										<b class="arrow"></b>
+									</li>									
+
+									<li class="">
+										<a href="../categorias/">
+											<i class="menu-icon fa fa-caret-right"></i>
+											Categorías
+										</a>
+
+										<b class="arrow"></b>
+									</li>
+									<li class="">
+										<a href="../marcas/">
+											<i class="menu-icon fa fa-caret-right"></i>
+											Marcas
+										</a>
+
+										<b class="arrow"></b>
+									</li>
+									<li class="">
+										<a href="../unidad_medida/">
+											<i class="menu-icon fa fa-caret-right"></i>
+											Unidades de Medida
+										</a>
+
+										<b class="arrow"></b>
+									</li>
+
+									<li class="">
+										<a href="../tipo_producto/">
+											<i class="menu-icon fa fa-caret-right"></i>
+											Tipo de producto
+										</a>
+
+										<b class="arrow"></b>
+									</li>									
+								</ul>
+							</li>
+
+							<li class="">
+								<a href="../empresa/">
+									<i class="menu-icon fa fa-caret-right"></i>
+									Empresa
+								</a>
+
+								<b class="arrow"></b>
+							</li>
+							<li class="">
+								<a href="../usuario/">
+									<i class="menu-icon fa fa-caret-right"></i>
+									Usuario
+								</a>
+
+								<b class="arrow"></b>
+							</li>							
+							<li class="">
+								<a href="../clientes/">
+									<i class="menu-icon fa fa-caret-right"></i>
+									Clientes
+								</a>
+
+								<b class="arrow"></b>
+							</li>
+							<li class="">
+								<a href="../proveedores/">
+									<i class="menu-icon fa fa-caret-right"></i>
+									Proveedores
+								</a>
+
+								<b class="arrow"></b>
+							</li>
+							<li class="">
+								<a href="../productos/">
+									<i class="menu-icon fa fa-caret-right"></i>
+									Productos
+								</a>
+
+								<b class="arrow"></b>
+							</li>
+							
+							
+						</ul>
+					</li>
+				</ul><!-- /.nav-list -->
+
+				<div class="sidebar-toggle sidebar-collapse" id="sidebar-collapse">
+					<i class="ace-icon fa fa-angle-double-left" data-icon1="ace-icon fa fa-angle-double-left" data-icon2="ace-icon fa fa-angle-double-right"></i>
+				</div>
+
+				<script type="text/javascript">
+					try{ace.settings.check("sidebar" , "collapsed")}catch(e){}
+				</script>
+			</div>
+	';
+}
+//pie de Pagina Footer 
+function footer(){
+	print'<div class="footer">
+				<div class="footer-inner">
+					<div class="footer-content">
+						<span class="bigger-120">
+							<span class="blue bolder">dow delopers</span>
+							Aplicación web facturación e inventario &copy; 2014-2015
+						</span>
+
+						&nbsp; &nbsp;
+						<span class="action-buttons">
+							<a href="#">
+								<i class="ace-icon fa fa-twitter-square light-blue bigger-150"></i>
+							</a>
+
+							<a href="#">
+								<i class="ace-icon fa fa-facebook-square text-primary bigger-150"></i>
+							</a>
+
+							<a href="#">
+								<i class="ace-icon fa fa-rss-square orange bigger-150"></i>
+							</a>
+						</span>
+					</div>
+				</div>
+			</div>';
+} 
+
+?>
---
+++ data/usuario/index.php
@@ -0,0 +1,327 @@
+<?php include('../menu/index.php'); ?>
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+		<meta charset="utf-8" />
+		<link rel="shortcut icon" href="../../dist/images/logo.fw.png">
+		<title>Inicio - <?php empresa(); ?></title>
+
+		<meta name="description" content="overview &amp; stats" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+
+		<!-- bootstrap & fontawesome -->
+		<link rel="stylesheet" href="../../dist/css/bootstrap.min.css" />
+		<link rel="stylesheet" href="../../dist/css/font-awesome.min.css" />
+		<!-- Select -->
+		<link rel="stylesheet" href="../../dist/css/chosen.min.css" />
+
+
+		<!-- page specific plugin styles -->
+
+		<!-- text fonts -->
+		<link rel="stylesheet" href="../../dist/css/fontdc.css" />
+
+		<!-- ace styles -->
+		<link rel="stylesheet" href="../../dist/css/ace.min.css" class="ace-main-stylesheet" id="main-ace-style" />
+
+		<!-- ace settings handler -->
+		<script src="../../dist/js/ace-extra.min.js"></script>
+	</head>
+
+	<body class="no-skin">
+		<?php menu_arriba(); ?>
+
+		<div class="main-container" id="main-container">
+			
+
+			<?php menu_lateral(); ?>
+
+			 <div class="main-content">
+				<div class="main-content-inner">
+					<div class="page-content">
+						<div class="row">
+							<div class="col-xs-12 col-sm-12 widget-container-col">
+								<div class="widget-box">
+									<div class="widget-header">
+										<h5 class="widget-title"><i class="ace-icon fa fa-user"></i> Usuarios</h5>
+
+										<div class="widget-toolbar">
+											
+
+											<a href="#" data-action="fullscreen" class="orange2">
+												<i class="ace-icon fa fa-expand"></i>
+											</a>
+
+											<a href="#" data-action="reload">
+												<i class="ace-icon fa fa-refresh"></i>
+											</a>
+
+											<a href="#" data-action="collapse">
+												<i class="ace-icon fa fa-chevron-up"></i>
+											</a>
+										</div>
+									</div>
+
+									<div class="widget-body">
+										<div class="widget-main">
+											<div class="row">
+												<form class="form-horizontal" role="form">
+													<div class="row">
+														<div class="col-xs-12">
+															<div class="col-sm-2">
+																<div class="widget-box">
+																	<div class="widget-header">
+																		<h4 class="widget-title">Imagen</h4>
+																	</div>
+
+																	<div class="widget-body">
+																		<div class="widget-main">
+																			<div class="form-group">
+																				<div class="col-xs-12">
+																					<input multiple="" type="file" class="txt_0" id="txt_0" name="txt_0" />
+																				</div>
+																			</div>
+																		</div>
+																	</div>
+																</div>
+
+															</div>
+															<div class="col-sm-5">
+																<div class="form-group">
+																	<label class="col-sm-3 control-label no-padding-right" for="form-field-1"> C.I.:</label>
+
+																	<div class="col-sm-9">
+																		<input type="text" id="txt_1" name="txt_1" placeholder="Cedula" class="form-control"/>
+																	</div>
+																</div>
+																<div class="form-group">
+																	<label class="col-sm-3 control-label no-padding-right" for="form-field-1"> Nombres: </label>
+
+																	<div class="col-sm-9">
+																		<input type="text" id="txt_2" name="txt_2" placeholder="Nombre Completo" class="form-control"  />
+																	</div>
+																</div>
+																<div class="form-group">
+																	<label class="col-sm-3 control-label no-padding-right" for="form-field-1"> Teléfono </label>
+
+																	<div class="col-sm-9">
+																		<input type="text" id="txt_3" name="txt_3" placeholder="Teléfono" class="form-control" />
+																	</div>
+																</div>
+																<div class="form-group">
+																	<label class="col-sm-3 control-label no-padding-right" for="form-field-1-1"> Cargo </label>
+
+																	<div class="col-sm-9">
+																		<select class="chosen-select form-control" id="txt_4" name="txt_4" data-placeholder="Pais">
+																			<option value="NM">New Mexico</option>
+																			<option value="NY">New York</option>													
+																		</select>
+																	</div>
+																</div>
+																<div class="form-group">
+																	<label class="col-sm-3 control-label no-padding-right" for="form-field-1-1"> Password </label>
+
+																	<div class="col-sm-9">
+																		<input type="text" id="txt_5" name="txt_5" placeholder="Password" class="form-control" />
+																	</div>
+																</div>
+																<div class="form-group">
+																	<label class="col-sm-3 control-label no-padding-right" for="form-field-1-1"> Repetir </label>
+
+																	<div class="col-sm-9">
+																		<input type="text" id="txt_6" name="txt_6" placeholder="Repetir Password" class="form-control" />
+																	</div>
+																</div>
+															</div>
+															<div class="col-sm-5">
+																<div class="form-group">
+																	<label class="col-sm-3 control-label no-padding-right" for="form-field-1-1"> Celular </label>
+
+																	<div class="col-sm-9">
+																		<input type="text" id="txt_7" name="txt_7" placeholder="Celular" class="form-control" />
+																	</div>
+																</div>
+																<div class="form-group">
+																	<label class="col-sm-3 control-label no-padding-right" for="form-field-1-1"> Correo </label>
+
+																	<div class="col-sm-9">
+																		<input type="text" id="txt_8" name="txt_8" placeholder="Correo" class="form-control" />
+																	</div>
+																</div>													
+																<div class="form-group">
+																	<label class="col-sm-3 control-label no-padding-right" for="form-field-1-1"> Pais </label>
+
+																	<div class="col-sm-9">
+																		<select class="chosen-select form-control" id="txt_9" name="txt_9" data-placeholder="Pais">
+																			<option value="NM">New Mexico</option>
+																			<option value="NY">New York</option>													
+																		</select>
+																	</div>
+																</div>
+																<div class="form-group">
+																	<label class="col-sm-3 control-label no-padding-right" for="form-field-1-1"> Provincia </label>
+
+																	<div class="col-sm-9">
+																		<select class="chosen-select form-control" id="txt_10" name="txt_10" data-placeholder="Provincia">
+																			<option value="NM">New Mexico</option>
+																			<option value="NY">New York</option>													
+																		</select>
+																	</div>
+																</div>
+																<div class="form-group">
+																	<label class="col-sm-3 control-label no-padding-right" for="form-field-1-1"> Ciudad </label>
+
+																	<div class="col-sm-9">
+																		<select class="chosen-select form-control" id="txt_11" name="txt_11" data-placeholder="Ciudad">
+																			<option value="NM">New Mexico</option>
+																			<option value="NY">New York</option>													
+																		</select>
+																	</div>
+																</div>
+																<div class="form-group">
+																	<label class="col-sm-3 control-label no-padding-right" for="form-field-1-1"> Dirección </label>
+
+																	<div class="col-sm-9">
+																		<input type="text" id="txt_12" name="txt_12" placeholder="Dirección" class="form-control" />
+																	</div>
+																</div>
+																
+															</div>
+														</div>
+													</div>
+													<h3 class="header smaller lighter green"></h3>
+													<div class="center">
+														<button type="submit" class="btn btn-white btn-info btn-bold">
+															<i class="ace-icon fa fa-floppy-o bigger-120 blue"></i>
+															Guardar
+														</button>
+														<button type="button" id="btn_0" class="btn btn-white btn-info btn-bold">
+															<i class="ace-icon fa fa-file-o bigger-120 blue"></i>
+															Limpiar
+														</button>
+														<button type="button" id="btn_1" class="btn btn-white btn-info btn-bold">
+															<i class="ace-icon fa fa-refresh bigger-120 blue"></i>
+															Actualizar
+														</button>
+														<button type="button" id="btn_2" class="btn btn-white btn-info btn-bold">
+															<i class="ace-icon fa fa-search bigger-120 blue"></i>
+															Buscar
+														</button>
+														<button type="button" id="btn_3" class="btn btn-white btn-info btn-bold">
+															<i class="ace-icon fa fa-arrow-circle-left bigger-120 blue"></i>
+															Atras
+														</button>
+														<button type="button" id="btn_4" class="btn btn-white btn-info btn-bold">
+															<i class="ace-icon fa fa fa-arrow-circle-right bigger-120 blue"></i>
+															Adelante
+														</button>
+													</div>
+												</form>
+											</div>									
+											
+
+											<div class="row">
+												<div class="col-xs-12">
+													<div class="center">														
+														
+																							
+													</div>																					
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>							
+						</div>
+					</div>
+				</div>
+			</div><!-- /.main-content -->
+
+			<?php footer(); ?>
+
+			<a href="#" id="btn-scroll-up" class="btn-scroll-up btn btn-sm btn-inverse">
+				<i class="ace-icon fa fa-angle-double-up icon-only bigger-110"></i>
+			</a>
+		</div><!-- /.main-container -->
+
+		<!-- basic scripts -->
+
+		
+		<!-- <![endif]-->
+
+		<!--[if IE]>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<![endif]-->
+
+		<!--[if !IE]> -->
+		<script type="text/javascript">
+			window.jQuery || document.write("<script src='../../dist/js/jquery.min.js'>"+"<"+"/script>");
+		</script>
+
+		<!-- <![endif]-->
+
+		<!--[if IE]>
+<script type="text/javascript">
+ window.jQuery || document.write("<script src='../../dist/js/jquery1x.min.js'>"+"<"+"/script>");
+</script>
+<![endif]-->
+		<script type="text/javascript">
+			if('ontouchstart' in document.documentElement) document.write("<script src='../../dist/js/jquery.mobile.custom.min.js'>"+"<"+"/script>");
+		</script>
+		<script src="../../dist/js/bootstrap.min.js"></script>
+
+		<!-- page specific plugin scripts -->
+
+		<!--[if lte IE 8]>
+		  <script src="../../dist/js/excanvas.min.js"></script>
+		<![endif]-->
+		<script src="../../dist/js/jquery-ui.custom.min.js"></script>
+		<script src="../../dist/js/jquery.ui.touch-punch.min.js"></script>
+		<script src="../../dist/js/jquery.easypiechart.min.js"></script>
+		<script src="../../dist/js/jquery.sparkline.min.js"></script>
+		<script src="../../dist/js/flot/jquery.flot.min.js"></script>
+		<script src="../../dist/js/flot/jquery.flot.pie.min.js"></script>
+		<script src="../../dist/js/flot/jquery.flot.resize.min.js"></script>
+		<script src="../../dist/js/chosen.jquery.min.js"></script>
+
+
+		<!-- ace scripts -->
+		<script src="../../dist/js/ace-elements.min.js"></script>
+		<script src="../../dist/js/ace.min.js"></script>
+
+		<!-- inline scripts related to this page -->
+		<script type="text/javascript">
+			jQuery(function($) {
+
+				$('#txt_0').ace_file_input({
+					style:'well',
+					btn_choose:'Seleccionar',
+					btn_change:null,
+					no_icon:'ace-icon fa fa-image',
+					droppable:true,
+					thumbnail:'small'
+				})
+				$('.chosen-select').chosen({allow_single_deselect:true}); 
+				$(window)
+					.off('resize.chosen')
+					.on('resize.chosen', function() {
+						$('.chosen-select').each(function() {
+							 var $this = $(this);
+							 $this.next().css({'width': $this.parent().width()});
+						})
+					}).trigger('resize.chosen');
+					//resize chosen on sidebar collapse/expand
+					$(document).on('settings.ace.chosen', function(e, event_name, event_val) {
+						if(event_name != 'sidebar_collapsed') return;
+						$('.chosen-select').each(function() {
+							 var $this = $(this);
+							 $this.next().css({'width': $this.parent().width()});
+						})
+					});
+
+
+			})
+		</script>
+	</body>
+</html>
