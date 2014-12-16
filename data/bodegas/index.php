20f2d7c1dcc17b13501facfd03a615bb850405e6
deividscriollo <deividscriollo@gmail.com>
2014-12-16 12:15:55 -0500

    banner

--- data/bodegas/index.php
+++ data/bodegas/index.php
@@ -35,6 +35,31 @@
 
             <div class="main-content">
                 <div class="main-content-inner">
+                    <div class="breadcrumbs" id="breadcrumbs">
+                        <script type="text/javascript">
+                            try{ace.settings.check('breadcrumbs' , 'fixed')}catch(e){}
+                        </script>
+
+                        <ul class="breadcrumb">
+                            <li>
+                                <i class="ace-icon fa fa-home home-icon"></i>
+                                <a href="../inicio/">Home</a>
+                            </li>
+                            <li class="active">Ingresos</li>
+                            <li class="active">Generales</li>
+                            <li class="active">Bodegas</li>
+                        </ul><!-- /.breadcrumb -->
+
+                        <div class="nav-search" id="nav-search">
+                            <form class="form-search">
+                                <span class="input-icon">
+                                    <input type="text" placeholder="Search ..." class="nav-search-input" id="nav-search-input" autocomplete="off" />
+                                    <i class="ace-icon fa fa-search nav-search-icon"></i>
+                                </span>
+                            </form>
+                        </div><!-- /.nav-search -->
+                    </div>
+
                     <div class="page-content">
                         <div class="row">
                             <div class="col-xs-12 col-sm-12 widget-container-col">
