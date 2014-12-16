9af66c9af9995e6a41ce20346d4db33f659ce2f9
deividscriollo <deividscriollo@gmail.com>
2014-12-16 09:57:21 -0500

    bodoga jqgrid

--- data/bodegas/index.php
+++ data/bodegas/index.php
@@ -4,21 +4,19 @@
 	<head>
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 		<meta charset="utf-8" />
-		<link rel="shortcut icon" href="../../dist/images/logo.fw.png">
-		<title>Bodegas - <?php empresa(); ?></title>
+		<title>jqGrid - Ace Admin</title>
 
-		<meta name="description" content="overview &amp; stats" />
+		<meta name="description" content="Dynamic tables and grids using jqGrid plugin" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
 
 		<!-- bootstrap & fontawesome -->
 		<link rel="stylesheet" href="../../dist/css/bootstrap.min.css" />
 		<link rel="stylesheet" href="../../dist/css/font-awesome.min.css" />
-		<!-- Especiales -->
-		<link rel="stylesheet" href="../../dist/css/ui.jqgrid.min.css" />
-
-
 
 		<!-- page specific plugin styles -->
+		<link rel="stylesheet" href="../../dist/css/jquery-ui.min.css" />
+		<link rel="stylesheet" href="../../dist/css/datepicker.min.css" />
+		<link rel="stylesheet" href="../../dist/css/ui.jqgrid.min.css" />
 
 		<!-- text fonts -->
 		<link rel="stylesheet" href="../../dist/css/fontdc.css" />
@@ -26,56 +24,192 @@
 		<!-- ace styles -->
 		<link rel="stylesheet" href="../../dist/css/ace.min.css" class="ace-main-stylesheet" id="main-ace-style" />
 
+		<!--[if lte IE 9]>
+			<link rel="stylesheet" href="../../dist/css/ace-part2.min.css" class="ace-main-stylesheet" />
+		<![endif]-->
+
+		<!--[if lte IE 9]>
+		  <link rel="stylesheet" href="../../dist/css/ace-ie.min.css" />
+		<![endif]-->
+
+		<!-- inline styles related to this page -->
+
 		<!-- ace settings handler -->
 		<script src="../../dist/js/ace-extra.min.js"></script>
+
+		<!-- HTML5shiv and Respond.js for IE8 to support HTML5 elements and media queries -->
+
+		<!--[if lte IE 8]>
+		<script src="../../dist/js/html5shiv.min.js"></script>
+		<script src="../../dist/js/respond.min.js"></script>
+		<![endif]-->
 	</head>
 
 	<body class="no-skin">
 		<?php menu_arriba(); ?>
 
 		<div class="main-container" id="main-container">
-			
-
 			<?php menu_lateral(); ?>
 
-			 <div class="main-content">
+			<div class="main-content">
 				<div class="main-content-inner">
-					<div class="page-content">
-						<div class="row">
-							<div class="col-xs-12 col-sm-12 widget-container-col">
-								<div class="widget-box">
-									<div class="widget-header">
-										<h5 class="widget-title"><i class="ace-icon fa fa-user"></i> Bodegas</h5>
+					<div class="breadcrumbs" id="breadcrumbs">
+						<script type="text/javascript">
+							try{ace.settings.check('breadcrumbs' , 'fixed')}catch(e){}
+						</script>
 
-										<div class="widget-toolbar">
-											
+						<ul class="breadcrumb">
+							<li>
+								<i class="ace-icon fa fa-home home-icon"></i>
+								<a href="#">Home</a>
+							</li>
 
-											<a href="#" data-action="fullscreen" class="orange2">
-												<i class="ace-icon fa fa-expand"></i>
-											</a>
+							<li>
+								<a href="#">Tables</a>
+							</li>
+							<li class="active">jqGrid plugin</li>
+						</ul><!-- /.breadcrumb -->
 
-											<a href="#" data-action="reload">
-												<i class="ace-icon fa fa-refresh"></i>
-											</a>
-
-											<a href="#" data-action="collapse">
-												<i class="ace-icon fa fa-chevron-up"></i>
-											</a>
-										</div>
-									</div>
-
-									<div class="widget-body">
-										<table id="grid-table"></table>
-										<div id="grid-pager"></div>										
-									</div>
-								</div>
-							</div>							
-						</div>
+						<div class="nav-search" id="nav-search">
+							<form class="form-search">
+								<span class="input-icon">
+									<input type="text" placeholder="Search ..." class="nav-search-input" id="nav-search-input" autocomplete="off" />
+									<i class="ace-icon fa fa-search nav-search-icon"></i>
+								</span>
+							</form>
+						</div><!-- /.nav-search -->
 					</div>
+
+					<div class="page-content">
+						<div class="ace-settings-container" id="ace-settings-container">
+							<div class="btn btn-app btn-xs btn-warning ace-settings-btn" id="ace-settings-btn">
+								<i class="ace-icon fa fa-cog bigger-130"></i>
+							</div>
+
+							<div class="ace-settings-box clearfix" id="ace-settings-box">
+								<div class="pull-left width-50">
+									<div class="ace-settings-item">
+										<div class="pull-left">
+											<select id="skin-colorpicker" class="hide">
+												<option data-skin="no-skin" value="#438EB9">#438EB9</option>
+												<option data-skin="skin-1" value="#222A2D">#222A2D</option>
+												<option data-skin="skin-2" value="#C6487E">#C6487E</option>
+												<option data-skin="skin-3" value="#D0D0D0">#D0D0D0</option>
+											</select>
+										</div>
+										<span>&nbsp; Choose Skin</span>
+									</div>
+
+									<div class="ace-settings-item">
+										<input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-navbar" />
+										<label class="lbl" for="ace-settings-navbar"> Fixed Navbar</label>
+									</div>
+
+									<div class="ace-settings-item">
+										<input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-sidebar" />
+										<label class="lbl" for="ace-settings-sidebar"> Fixed Sidebar</label>
+									</div>
+
+									<div class="ace-settings-item">
+										<input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-breadcrumbs" />
+										<label class="lbl" for="ace-settings-breadcrumbs"> Fixed Breadcrumbs</label>
+									</div>
+
+									<div class="ace-settings-item">
+										<input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-rtl" />
+										<label class="lbl" for="ace-settings-rtl"> Right To Left (rtl)</label>
+									</div>
+
+									<div class="ace-settings-item">
+										<input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-add-container" />
+										<label class="lbl" for="ace-settings-add-container">
+											Inside
+											<b>.container</b>
+										</label>
+									</div>
+								</div><!-- /.pull-left -->
+
+								<div class="pull-left width-50">
+									<div class="ace-settings-item">
+										<input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-hover" />
+										<label class="lbl" for="ace-settings-hover"> Submenu on Hover</label>
+									</div>
+
+									<div class="ace-settings-item">
+										<input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-compact" />
+										<label class="lbl" for="ace-settings-compact"> Compact Sidebar</label>
+									</div>
+
+									<div class="ace-settings-item">
+										<input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-highlight" />
+										<label class="lbl" for="ace-settings-highlight"> Alt. Active Item</label>
+									</div>
+								</div><!-- /.pull-left -->
+							</div><!-- /.ace-settings-box -->
+						</div><!-- /.ace-settings-container -->
+
+						<div class="page-header">
+							<h1>
+								jqGrid
+								<small>
+									<i class="ace-icon fa fa-angle-double-right"></i>
+									Dynamic tables and grids using jqGrid plugin
+								</small>
+							</h1>
+						</div><!-- /.page-header -->
+
+						<div class="row">
+							<div class="col-xs-12">
+								<!-- PAGE CONTENT BEGINS -->
+								<div class="alert alert-info">
+									<button class="close" data-dismiss="alert">
+										<i class="ace-icon fa fa-times"></i>
+									</button>
+
+									<i class="ace-icon fa fa-hand-o-right"></i>
+									Please note that demo server is not configured to save the changes, therefore you may see an error message.
+								</div>
+
+								<table id="grid-table"></table>
+
+								<div id="grid-pager"></div>
+
+								<script type="text/javascript">
+									var $path_base = ".";//in Ace demo this will be used for editurl parameter
+								</script>
+
+								<!-- PAGE CONTENT ENDS -->
+							</div><!-- /.col -->
+						</div><!-- /.row -->
+					</div><!-- /.page-content -->
 				</div>
 			</div><!-- /.main-content -->
 
-			<?php footer(); ?>
+			<div class="footer">
+				<div class="footer-inner">
+					<div class="footer-content">
+						<span class="bigger-120">
+							<span class="blue bolder">Ace</span>
+							Application &copy; 2013-2014
+						</span>
+
+						&nbsp; &nbsp;
+						<span class="action-buttons">
+							<a href="#">
+								<i class="ace-icon fa fa-twitter-square light-blue bigger-150"></i>
+							</a>
+
+							<a href="#">
+								<i class="ace-icon fa fa-facebook-square text-primary bigger-150"></i>
+							</a>
+
+							<a href="#">
+								<i class="ace-icon fa fa-rss-square orange bigger-150"></i>
+							</a>
+						</span>
+					</div>
+				</div>
+			</div>
 
 			<a href="#" id="btn-scroll-up" class="btn-scroll-up btn btn-sm btn-inverse">
 				<i class="ace-icon fa fa-angle-double-up icon-only bigger-110"></i>
@@ -85,6 +219,7 @@
 		<!-- basic scripts -->
 
 		
+
 		<!-- <![endif]-->
 
 		<!--[if IE]>
@@ -109,32 +244,16 @@
 		<script src="../../dist/js/bootstrap.min.js"></script>
 
 		<!-- page specific plugin scripts -->
-
-		<!--[if lte IE 8]>
-		  <script src="../../dist/js/excanvas.min.js"></script>
-		<![endif]-->
-		<script src="../../dist/js/jquery-ui.custom.min.js"></script>
-		<script src="../../dist/js/jquery.ui.touch-punch.min.js"></script>
-
-
+		<script src="../../dist/js/date-time/bootstrap-datepicker.min.js"></script>
 		<script src="../../dist/js/jqGrid/jquery.jqGrid.min.js"></script>
 		<script src="../../dist/js/jqGrid/i18n/grid.locale-en.js"></script>
-
-
 
 		<!-- ace scripts -->
 		<script src="../../dist/js/ace-elements.min.js"></script>
 		<script src="../../dist/js/ace.min.js"></script>
-		
-		<script src="../generales.js"></script>
 
 		<!-- inline scripts related to this page -->
-
-	
-
-	</body>
-</html>  
-<script type="text/javascript">
+		<script type="text/javascript">
 			var grid_data = 
 			[ 
 				{id:"1",name:"Desktop Computer",note:"note",stock:"Yes",ship:"FedEx", sdate:"2007-12-03"},
@@ -537,3 +656,5 @@
 				});
 			});
 		</script>
+	</body>
+</html>
