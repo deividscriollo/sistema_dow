d347341438af6366a509dde776d679ce79a87b0b
Oskrin <oskrs11@hotmail.com>
2014-12-16 11:57:47 -0500

    bodegas

--- dist/js/jqGrid/i18n/grid.locale-en.js
+++ dist/js/jqGrid/i18n/grid.locale-en.js
@@ -10,81 +10,81 @@
 $.jgrid = $.jgrid || {};
 $.extend($.jgrid,{
 	defaults : {
-		recordtext: "View {0} - {1} of {2}",
-		emptyrecords: "No records to view",
+		recordtext: "Mostrando {0} - {1} de {2}",
+		emptyrecords: "Sin registros que mostrar",
 		loadtext: "Loading...",
-		pgtext : "Page {0} of {1}"
+		pgtext : "Página {0} de {1}"
 	},
 	search : {
-		caption: "Search...",
-		Find: "Find",
-		Reset: "Reset",
-		odata: [{ oper:'eq', text:'equal'},{ oper:'ne', text:'not equal'},{ oper:'lt', text:'less'},{ oper:'le', text:'less or equal'},{ oper:'gt', text:'greater'},{ oper:'ge', text:'greater or equal'},{ oper:'bw', text:'begins with'},{ oper:'bn', text:'does not begin with'},{ oper:'in', text:'is in'},{ oper:'ni', text:'is not in'},{ oper:'ew', text:'ends with'},{ oper:'en', text:'does not end with'},{ oper:'cn', text:'contains'},{ oper:'nc', text:'does not contain'},{ oper:'nu', text:'is null'},{ oper:'nn', text:'is not null'}],
-		groupOps: [{ op: "AND", text: "all" },{ op: "OR",  text: "any" }],
-		operandTitle : "Click to select search operation.",
-		resetTitle : "Reset Search Value"
+		caption: "Búsqueda...",
+		Find: "Buscar",
+		Reset: "Limpiar",
+		odata: [{ oper:'eq', text:'Igual'},{ oper:'ne', text:'no igual a'},{ oper:'lt', text:'less'},{ oper:'le', text:'less or equal'},{ oper:'gt', text:'greater'},{ oper:'ge', text:'greater or equal'},{ oper:'bw', text:'begins with'},{ oper:'bn', text:'does not begin with'},{ oper:'in', text:'is in'},{ oper:'ni', text:'is not in'},{ oper:'ew', text:'ends with'},{ oper:'en', text:'does not end with'},{ oper:'cn', text:'contains'},{ oper:'nc', text:'does not contain'},{ oper:'nu', text:'is null'},{ oper:'nn', text:'is not null'}],
+		groupOps: [{ op: "AND", text: "Todos" },{ op: "OR",  text: "Cualquier" }],
+		operandTitle : "Haga clic para seleccionar la operación de búsqueda.",
+		resetTitle : "Reiniciar la Búsqueda"
 	},
 	edit : {
-		addCaption: "Add Record",
-		editCaption: "Edit Record",
-		bSubmit: "Submit",
-		bCancel: "Cancel",
+		addCaption: "Agregar Registro",
+		editCaption: "Modificar Registro",
+		bSubmit: "Guardar",
+		bCancel: "Cancelar",
 		bClose: "Close",
-		saveData: "Data has been changed! Save changes?",
-		bYes : "Yes",
+		saveData: "Guardar los cambios?",
+		bYes : "Si",
 		bNo : "No",
-		bExit : "Cancel",
+		bExit : "Cancelar",
 		msg: {
-			required:"Field is required",
-			number:"Please, enter valid number",
-			minValue:"value must be greater than or equal to ",
-			maxValue:"value must be less than or equal to",
-			email: "is not a valid e-mail",
-			integer: "Please, enter valid integer value",
-			date: "Please, enter valid date value",
-			url: "is not a valid URL. Prefix required ('http://' or 'https://')",
-			nodefined : " is not defined!",
-			novalue : " return value is required!",
-			customarray : "Custom function should return array!",
-			customfcheck : "Custom function should be present in case of custom checking!"
+			required:"Se requiere Campo",
+			number:"Por favor, ingrese un número válido",
+			minValue:"Valor debe ser mayor que o igual a",
+			maxValue:"Valor debe ser menor que o igual a",
+			email: "E-mail incorrecto",
+			integer: "Por favor, introduzca un valor entero válido",
+			date: "Por favor, introduzca una fecha válida",
+			url: "No es una URL válida ('http://' or 'https://')",
+			nodefined : "No se define!",
+			novalue : " Se requiere un valor de retorno!",
+			customarray : "Función personalizada debe devolver array!",
+			customfcheck : "Función personalizada debe estar presente en caso de comprobación!"
 			
 		}
 	},
 	view : {
-		caption: "View Record",
+		caption: "Consultar Registro",
 		bClose: "Close"
 	},
 	del : {
-		caption: "Delete",
-		msg: "Delete selected record(s)?",
-		bSubmit: "Delete",
-		bCancel: "Cancel"
+		caption: "Eliminar",
+		msg: "Eliminar registro seleccionado(s)?",
+		bSubmit: "Eliminar",
+		bCancel: "Cancelar"
 	},
 	nav : {
 		edittext: "",
-		edittitle: "Edit selected row",
+		edittitle: "Modificar fila selecionada",
 		addtext:"",
-		addtitle: "Add new row",
+		addtitle: "Agregar nuevo registro",
 		deltext: "",
-		deltitle: "Delete selected row",
+		deltitle: "Eliminar fila selecionada",
 		searchtext: "",
-		searchtitle: "Find records",
+		searchtitle: "Buscar Información",
 		refreshtext: "",
-		refreshtitle: "Reload Grid",
-		alertcap: "Warning",
-		alerttext: "Please, select row",
+		refreshtitle: "Recargar datos",
+		alertcap: "Aviso",
+		alerttext: "Por favor, seleccione una fila",
 		viewtext: "",
-		viewtitle: "View selected row"
+		viewtitle: "Ver fila selecionada"
 	},
 	col : {
-		caption: "Select columns",
+		caption: "Seleccione una fila",
 		bSubmit: "Ok",
-		bCancel: "Cancel"
+		bCancel: "Cancelar"
 	},
 	errors : {
 		errcap : "Error",
 		nourl : "No url is set",
-		norecords: "No records to process",
+		norecords: "No hay registros que procesar",
 		model : "Length of colNames <> colModel!"
 	},
 	formatter : {
@@ -106,57 +106,16 @@ $.extend($.jgrid,{
 			newformat: 'n/j/Y',
 			parseRe : /[#%\\\/:_;.,\t\s-]/,
 			masks : {
-				// see http://php.net/manual/en/function.date.php for PHP format used in jqGrid
-				// and see http://docs.jquery.com/UI/Datepicker/formatDate
-				// and https://github.com/jquery/globalize#dates for alternative formats used frequently
-				// one can find on https://github.com/jquery/globalize/tree/master/lib/cultures many
-				// information about date, time, numbers and currency formats used in different countries
-				// one should just convert the information in PHP format
 				ISO8601Long:"Y-m-d H:i:s",
 				ISO8601Short:"Y-m-d",
-				// short date:
-				//    n - Numeric representation of a month, without leading zeros
-				//    j - Day of the month without leading zeros
-				//    Y - A full numeric representation of a year, 4 digits
-				// example: 3/1/2012 which means 1 March 2012
-				ShortDate: "n/j/Y", // in jQuery UI Datepicker: "M/d/yyyy"
-				// long date:
-				//    l - A full textual representation of the day of the week
-				//    F - A full textual representation of a month
-				//    d - Day of the month, 2 digits with leading zeros
-				//    Y - A full numeric representation of a year, 4 digits
-				LongDate: "l, F d, Y", // in jQuery UI Datepicker: "dddd, MMMM dd, yyyy"
-				// long date with long time:
-				//    l - A full textual representation of the day of the week
-				//    F - A full textual representation of a month
-				//    d - Day of the month, 2 digits with leading zeros
-				//    Y - A full numeric representation of a year, 4 digits
-				//    g - 12-hour format of an hour without leading zeros
-				//    i - Minutes with leading zeros
-				//    s - Seconds, with leading zeros
-				//    A - Uppercase Ante meridiem and Post meridiem (AM or PM)
-				FullDateTime: "l, F d, Y g:i:s A", // in jQuery UI Datepicker: "dddd, MMMM dd, yyyy h:mm:ss tt"
-				// month day:
-				//    F - A full textual representation of a month
-				//    d - Day of the month, 2 digits with leading zeros
-				MonthDay: "F d", // in jQuery UI Datepicker: "MMMM dd"
-				// short time (without seconds)
-				//    g - 12-hour format of an hour without leading zeros
-				//    i - Minutes with leading zeros
-				//    A - Uppercase Ante meridiem and Post meridiem (AM or PM)
-				ShortTime: "g:i A", // in jQuery UI Datepicker: "h:mm tt"
-				// long time (with seconds)
-				//    g - 12-hour format of an hour without leading zeros
-				//    i - Minutes with leading zeros
-				//    s - Seconds, with leading zeros
-				//    A - Uppercase Ante meridiem and Post meridiem (AM or PM)
-				LongTime: "g:i:s A", // in jQuery UI Datepicker: "h:mm:ss tt"
-				SortableDateTime: "Y-m-d\\TH:i:s",
+				ShortDate: "n/j/Y", 
+				LongDate: "l, F d, Y",
+				FullDateTime: "l, F d, Y g:i:s A",
+				MonthDay: "F d", 
+				ShortTime: "g:i A", 
+					SortableDateTime: "Y-m-d\\TH:i:s",
 				UniversalSortableDateTime: "Y-m-d H:i:sO",
-				// month with year
-				//    Y - A full numeric representation of a year, 4 digits
-				//    F - A full textual representation of a month
-				YearMonth: "F, Y" // in jQuery UI Datepicker: "MMMM, yyyy"
+				YearMonth: "F, Y"
 			},
 			reformatAfterEdit : false
 		},
