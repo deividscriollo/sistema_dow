bfcb66581527832ef610a7b117af77f5bf4d2079
deividscriollo <deividscriollo@gmail.com>
2014-12-18 16:58:50 -0500

    formulario clientes

--- data/clientes/index.php
+++ data/clientes/index.php
@@ -15,7 +15,7 @@
 		<link rel="stylesheet" href="../../dist/css/font-awesome.min.css" />
 		<!-- Select -->
 		<link rel="stylesheet" href="../../dist/css/chosen.min.css" />
-
+		<link rel="stylesheet" href="../../dist/css/ui.jqgrid.min.css" />
 
 		<!-- page specific plugin styles -->
 
@@ -39,29 +39,30 @@
 
 			 <div class="main-content">
 				<div class="main-content-inner">
+					<div class="breadcrumbs" id="breadcrumbs">
+                        <script type="text/javascript">
+                            try{ace.settings.check('breadcrumbs' , 'fixed')}catch(e){}
+                        </script>
+
+                        <ul class="breadcrumb">
+                            <li>
+                                <i class="ace-icon fa fa-home home-icon"></i>
+                                <a href="../inicio/">Inicio</a>
+                            </li>
+                            <li class="active">Ingresos</li>
+                            <li class="active">Clientes</li>
+                            
+                        </ul>
+                    </div>
 					<div class="page-content">
 						<div class="row">
 							<div class="col-xs-12 col-sm-12 widget-container-col">
 								<div class="widget-box">
 									<div class="widget-header">
-										<h5 class="widget-title">Proceso</h5>
+										<h5 class="widget-title"><i class="ace-icon fa fa-user"></i> Clientes</h5>
 
 										<div class="widget-toolbar">
-											<div class="widget-menu">
-												<a href="#" data-action="settings" data-toggle="dropdown">
-													<i class="ace-icon fa fa-bars"></i>
-												</a>
-
-												<ul class="dropdown-menu dropdown-menu-right dropdown-light-blue dropdown-caret dropdown-closer">
-													<li>
-														<a data-toggle="tab" href="#dropdown1">Opcion 1</a>
-													</li>
-
-													<li>
-														<a data-toggle="tab" href="#dropdown2">Opcion 2</a>
-													</li>
-												</ul>
-											</div>
+											
 
 											<a href="#" data-action="fullscreen" class="orange2">
 												<i class="ace-icon fa fa-expand"></i>
@@ -74,17 +75,151 @@
 											<a href="#" data-action="collapse">
 												<i class="ace-icon fa fa-chevron-up"></i>
 											</a>
-
-											<a href="#" data-action="close">
-												<i class="ace-icon fa fa-times"></i>
-											</a>
 										</div>
 									</div>
 
 									<div class="widget-body">
 										<div class="widget-main">
+											<div class="row">
+												<form class="form-horizontal" role="form" rol="form" action="" method="POST" id="form_usuario">												
+													<div class="row">
+														<div class="col-xs-12">															
+															<div class="col-sm-5">
+																<div class="form-group">
+																	<label class="col-sm-3 control-label no-padding-right" for="form-field-1-1"> Tipo Documento: </label>
 
-											aki codigo
+																	<div class="col-sm-9">
+																		<select class="chosen-select form-control" id="txt_9" name="txt_9" data-placeholder="País">
+																			
+																			
+																		</select>																	
+																	</div>
+																</div>
+																<div class="form-group has-error">
+																	<label class="col-sm-3 control-label no-padding-right" for="form-field-1"> C.I.:</label>
+																	<div class="col-sm-9">
+																		<input type="text" id="txt_1" name="txt_1"  placeholder="Cedula" class="form-control" data-toggle="tooltip" data-original-title="Agregue el nro de CI en caso de ser extranjero seleccione la casilla Extranjero" required pattern="[0-9]{1,}" />
+																																				
+																	</div>
+																</div>
+																<div class="form-group has-error">
+																	<label class="col-sm-3 control-label no-padding-right" for="form-field-1"> Nombres : </label>
+																	<div class="col-sm-9">
+																		<input type="text" id="txt_2" name="txt_2" placeholder="Nombre Completo" class="form-control" required pattern="[A-Za-záéíóúÁÉÍÓÚñÑ0-9]{1,}" data-toggle="tooltip" data-original-title="Nombres completos"  />
+																	</div>
+																</div>
+																<div class="form-group">
+																	<label class="col-sm-3 control-label no-padding-right" for="form-field-1"> Teléfono: </label>
+
+																	<div class="col-sm-9">
+																		<input type="text" id="txt_3" name="txt_3" placeholder="Teléfono" class="form-control" />
+																	</div>
+																</div>
+																<div class="form-group">
+																	<label class="col-sm-3 control-label no-padding-right" for="form-field-1-1"> Celular: </label>
+																	<div class="col-sm-9">																																				
+																			<input type="text" id="txt_7" name="txt_7" placeholder="Celular" class="form-control" />																																		
+																	</div>
+																</div>
+																<div class="form-group">
+																	<label class="col-sm-3 control-label no-padding-right" for="form-field-1-1"> Correo: </label>
+
+																	<div class="col-sm-9">
+																		<input type="mail" id="txt_8" name="txt_8" placeholder="Correo" class="form-control" />
+																	</div>
+																</div>														
+															</div>
+															<div class="col-sm-5">
+																
+																
+																<div class="form-group">
+																	<label class="col-sm-3 control-label no-padding-right" for="form-field-1-1"> Tipo Cliente: </label>
+
+																	<div class="col-sm-9">
+																		<select class="chosen-select form-control" id="txt_4" name="txt_4" data-placeholder="Tipo cliente">																			
+																		</select>
+																	</div>
+																</div>											
+																<div class="form-group">
+																	<label class="col-sm-3 control-label no-padding-right" for="form-field-1-1"> País: </label>
+
+																	<div class="col-sm-9">
+																		<select class="chosen-select form-control" id="txt_9" name="txt_9" data-placeholder="País">
+																			
+																			
+																		</select>																	
+																	</div>
+																</div>
+																<div class="form-group">
+																	<label class="col-sm-3 control-label no-padding-right" for="form-field-1-1"> Provincia: </label>
+
+																	<div class="col-sm-9">
+																		<select class="chosen-select form-control" id="txt_10" name="txt_10" data-placeholder="Provincia">
+																															
+																		</select>
+																	</div>
+																</div>
+																<div class="form-group">
+																	<label class="col-sm-3 control-label no-padding-right" for="form-field-1-1"> Ciudad: </label>
+
+																	<div class="col-sm-9">
+																		<select class="chosen-select form-control" id="txt_11" name="txt_11" data-placeholder="Ciudad">
+																															
+																		</select>
+																	</div>
+																</div>
+																<div class="form-group has-error">
+																	<label class="col-sm-3 control-label no-padding-right" for="form-field-1-1"> Dirección: </label>
+
+																	<div class="col-sm-9">
+																		<input type="text" id="txt_12" name="txt_12" placeholder="Dirección" class="form-control"  required pattern="[A-Za-záéíóúÁÉÍÓÚñÑ0-9]{1,}" data-toggle="tooltip" data-original-title="Ingrese la dirección del usuario a crear" />
+																	</div>
+																</div>
+																<div class="form-group has-error">
+																	<label class="col-sm-3 control-label no-padding-right" for="form-field-1-1"> Otros: </label>
+																	<div class="col-sm-9">																		
+																		<textarea placeholder="otros" rows="3" class="form-control" >
+																			
+																		</textarea>
+																	</div>
+																</div>
+
+																
+															</div>
+														</div>
+													</div>
+													<h3 class="header smaller lighter green"></h3>
+													<div class="center">
+														<button type="button" class="btn btn-white btn-info btn-bold" id="btn_0">
+															<i class="ace-icon fa fa-floppy-o bigger-120 blue"></i>
+															Guardar
+														</button>
+														<button type="button" id="btn_1" class="btn btn-white btn-info btn-bold">
+															<i class="ace-icon fa fa-file-o bigger-120 blue"></i>
+															Limpiar
+														</button>
+														<button type="button" id="btn_2" class="btn btn-white btn-info btn-bold">
+															<i class="ace-icon fa fa-refresh bigger-120 blue"></i>
+															Actualizar
+														</button>														
+														<button data-toggle="modal" href="#myModal" type="button" id="btn_3" class="btn btn-white btn-info btn-bold">
+															<i class="ace-icon fa fa-search bigger-120 blue"></i>
+															Buscar
+														</button>
+														<button type="button" id="btn_4" class="btn btn-white btn-info btn-bold">
+															<i class="ace-icon fa fa-arrow-circle-left bigger-120 blue"></i>
+															Atras
+														</button>
+														<button type="button" id="btn_5" class="btn btn-white btn-info btn-bold">
+															<i class="ace-icon fa fa fa-arrow-circle-right bigger-120 blue"></i>
+															Adelante
+														</button>
+													</div>
+												</form>
+											</div>									
+											
+
+											
 										</div>
 									</div>
 								</div>
@@ -142,13 +277,41 @@
 		<script src="../../dist/js/chosen.jquery.min.js"></script>
 
 
+
 		<!-- ace scripts -->
 		<script src="../../dist/js/ace-elements.min.js"></script>
 		<script src="../../dist/js/ace.min.js"></script>
+		<script src="../../dist/js/jqGrid/jquery.jqGrid.min.js"></script>
+        <script src="../../dist/js/jqGrid/i18n/grid.locale-en.js"></script>
+		
+		<script src="../generales.js"></script>
 
 		<!-- inline scripts related to this page -->
-		<script type="text/javascript">
-			
-		</script>
+
+	
+
 	</body>
-</html>
+</html>  
+  <!-- Modal -->
+  <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+          <h4 class="modal-title">BUSCAR USUARIOS</h4>
+        </div>
+        <div class="modal-body">
+            <table id="table"></table>
+			<div id="pager"></div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+  </div><!-- /.modal -->
+
+<script type="text/javascript">
+	
+</script>
