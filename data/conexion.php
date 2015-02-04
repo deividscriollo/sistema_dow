e9002076f6d546c16fb6be5ec57d94ddb4ccdb00
Willy Narvaez <willynarvaez6@gmail.com>
2015-02-04 18:00:28 -0500

    cambios

--- data/conexion.php
+++ data/conexion.php
@@ -4,7 +4,7 @@
     	$conexion = null;
         try
         {                             
-         $conexion = pg_connect("host=localhost dbname=aplicacion_dow port=5432 user=postgres password=rootdow");
+         //$conexion = pg_connect("host=localhost dbname=aplicacion_dow port=5432 user=postgres password=rootdow");
          if( $conexion == false )
                  throw new Exception( "Error PostgreSQL ".pg_last_error() );
         }
