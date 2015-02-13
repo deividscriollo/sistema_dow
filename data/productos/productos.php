6151d7d79970ed6812524a16bca5bd370bca4c18
Oskrin <oskrs11@hotmail.com>
2015-02-13 14:20:57 -0500

    gh
    gh

--- data/productos/productos.php
+++ data/productos/productos.php
@@ -45,15 +45,18 @@
 	    	}	
     	}    	    		    	
     	if($data == 0){
+    		$valor1 = number_format($_POST['txt_9'], 2, '.', '');
+			$valor2 = number_format($_POST['txt_4'], 2, '.', '');
+			$valor3 = number_format($_POST['txt_11'], 2, '.', '');
 			if (strpos($cadena, $buscar) ==  FALSE) {
-				$sql ="insert into productos values ('$id','$_POST[txt_1]','$_POST[txt_8]','".strtoupper($_POST['txt_2'])."','$_POST[txt_9]','$_POST[txt_3]','$_POST[txt_10]','$_POST[txt_4]','$_POST[txt_11]','$_POST[txt_5]','$_POST[txt_12]','$_POST[txt_6]','$_POST[txt_13]','$_POST[txt_7]','$_POST[txt_14]','$sin_existencia','$_POST[txt_16]','$_POST[txt_17]','$producto_series','$expiracion_producto','$_POST[txt_18]','default.png','$producto_activo','$fecha','$iva_producto')";					
+				$sql ="insert into productos values ('$id','$_POST[txt_1]','$_POST[txt_8]','".strtoupper($_POST['txt_2'])."','$valor1','$_POST[txt_3]','$_POST[txt_10]','$valor2','$valor3','$_POST[txt_5]','$_POST[txt_12]','$_POST[txt_6]','$_POST[txt_13]','$_POST[txt_7]','$_POST[txt_14]','$sin_existencia','$_POST[txt_16]','$_POST[txt_17]','$producto_series','$expiracion_producto','$_POST[txt_18]','default.png','$producto_activo','$fecha','$iva_producto')";					
 			}else{					
 				$resp = img_64("img",$_POST['img'],'png',$id);					
 				if($resp == "true"){
-					$sql ="insert into productos values ('$id','$_POST[txt_1]','$_POST[txt_8]','".strtoupper($_POST['txt_2'])."','$_POST[txt_9]','$_POST[txt_3]','$_POST[txt_10]','$_POST[txt_4]','$_POST[txt_11]','$_POST[txt_5]','$_POST[txt_12]','$_POST[txt_6]','$_POST[txt_13]','$_POST[txt_7]','$_POST[txt_14]','$sin_existencia','$_POST[txt_16]','$_POST[txt_17]','$producto_series','$expiracion_producto','$_POST[txt_18]','".$id.".png','$producto_activo','$fecha','$iva_producto')";			
+					$sql ="insert into productos values ('$id','$_POST[txt_1]','$_POST[txt_8]','".strtoupper($_POST['txt_2'])."','$valor1','$_POST[txt_3]','$_POST[txt_10]','$valor2','$valor3','$_POST[txt_5]','$_POST[txt_12]','$_POST[txt_6]','$_POST[txt_13]','$_POST[txt_7]','$_POST[txt_14]','$sin_existencia','$_POST[txt_16]','$_POST[txt_17]','$producto_series','$expiracion_producto','$_POST[txt_18]','".$id.".png','$producto_activo','$fecha','$iva_producto')";			
 				}
 				else{
-					$sql ="insert into productos values ('$id','$_POST[txt_1]','$_POST[txt_8]','".strtoupper($_POST['txt_2'])."','$_POST[txt_9]','$_POST[txt_3]','$_POST[txt_10]','$_POST[txt_4]','$_POST[txt_11]','$_POST[txt_5]','$_POST[txt_12]','$_POST[txt_6]','$_POST[txt_13]','$_POST[txt_7]','$_POST[txt_14]','$sin_existencia','$_POST[txt_16]','$_POST[txt_17]','$producto_series','$expiracion_producto','$_POST[txt_18]','default.png','$producto_activo','$fecha','$iva_producto')";		
+					$sql ="insert into productos values ('$id','$_POST[txt_1]','$_POST[txt_8]','".strtoupper($_POST['txt_2'])."','$valor1','$_POST[txt_3]','$_POST[txt_10]','$valor2','$valor3','$_POST[txt_5]','$_POST[txt_12]','$_POST[txt_6]','$_POST[txt_13]','$_POST[txt_7]','$_POST[txt_14]','$sin_existencia','$_POST[txt_16]','$_POST[txt_17]','$producto_series','$expiracion_producto','$_POST[txt_18]','default.png','$producto_activo','$fecha','$iva_producto')";		
 				}
 			}
 			$guardar = guardarSql($conexion,$sql);
@@ -82,15 +85,18 @@
     	}    	
 
 		if($data == 0){
+			$valor1 = number_format($_POST['txt_9'], 2, '.', '');
+			$valor2 = number_format($_POST['txt_4'], 2, '.', '');
+			$valor3 = number_format($_POST['txt_11'], 2, '.', '');
     		if (strpos($cadena, $buscar) ==  FALSE) {
-				$sql = "update productos set codigo='$_POST[txt_1]',codigo_barras='$_POST[txt_8]',descripcion='".strtoupper($_POST['txt_2'])."',precio='$_POST[txt_9]',utilidad_minorista='$_POST[txt_3]',utilidad_mayorista='$_POST[txt_10]',precio_minorista='$_POST[txt_4]',precio_mayorista='$_POST[txt_11]',id_tipo='$_POST[txt_5]',stock='$_POST[txt_12]',id_categoria='$_POST[txt_6]',id_marca='$_POST[txt_13]',id_bodega='$_POST[txt_7]',id_unidad='$_POST[txt_14]',facturar_existencia='$sin_existencia',cantidad_minima='$_POST[txt_16]',cantidad_maxima='$_POST[txt_17]',id_series_venta='$producto_series',id_lotes='$expiracion_producto',comentario='$_POST[txt_18]',imagen='default.png',estado='$producto_activo',iva_producto='$iva_producto' where id_productos = '$_POST[txt_0]'";					
+				$sql = "update productos set codigo='$_POST[txt_1]',codigo_barras='$_POST[txt_8]',descripcion='".strtoupper($_POST['txt_2'])."',precio='$valor1',utilidad_minorista='$_POST[txt_3]',utilidad_mayorista='$_POST[txt_10]',precio_minorista='$valor2',precio_mayorista='$valor3',id_tipo='$_POST[txt_5]',stock='$_POST[txt_12]',id_categoria='$_POST[txt_6]',id_marca='$_POST[txt_13]',id_bodega='$_POST[txt_7]',id_unidad='$_POST[txt_14]',facturar_existencia='$sin_existencia',cantidad_minima='$_POST[txt_16]',cantidad_maxima='$_POST[txt_17]',id_series_venta='$producto_series',id_lotes='$expiracion_producto',comentario='$_POST[txt_18]',imagen='default.png',estado='$producto_activo',iva_producto='$iva_producto' where id_productos = '$_POST[txt_0]'";					
 			}else{					
 				$resp = img_64("img",$_POST['img'],'png',$id);					
 				if($resp == "true"){				
-					$sql = "update productos set codigo='$_POST[txt_1]',codigo_barras='$_POST[txt_8]',descripcion='".strtoupper($_POST['txt_2'])."',precio='$_POST[txt_9]',utilidad_minorista='$_POST[txt_3]',utilidad_mayorista='$_POST[txt_10]',precio_minorista='$_POST[txt_4]',precio_mayorista='$_POST[txt_11]',id_tipo='$_POST[txt_5]',stock='$_POST[txt_12]',id_categoria='$_POST[txt_6]',id_marca='$_POST[txt_13]',id_bodega='$_POST[txt_7]',id_unidad='$_POST[txt_14]',facturar_existencia='$sin_existencia',cantidad_minima='$_POST[txt_16]',cantidad_maxima='$_POST[txt_17]',id_series_venta='$producto_series',id_lotes='$expiracion_producto',comentario='$_POST[txt_18]',imagen='".$id.".png',estado='$producto_activo',iva_producto='$iva_producto' where id_productos = '$_POST[txt_0]'";								
+					$sql = "update productos set codigo='$_POST[txt_1]',codigo_barras='$_POST[txt_8]',descripcion='".strtoupper($_POST['txt_2'])."',precio='$valor1',utilidad_minorista='$_POST[txt_3]',utilidad_mayorista='$_POST[txt_10]',precio_minorista='$valor2',precio_mayorista='$valor3',id_tipo='$_POST[txt_5]',stock='$_POST[txt_12]',id_categoria='$_POST[txt_6]',id_marca='$_POST[txt_13]',id_bodega='$_POST[txt_7]',id_unidad='$_POST[txt_14]',facturar_existencia='$sin_existencia',cantidad_minima='$_POST[txt_16]',cantidad_maxima='$_POST[txt_17]',id_series_venta='$producto_series',id_lotes='$expiracion_producto',comentario='$_POST[txt_18]',imagen='".$id.".png',estado='$producto_activo',iva_producto='$iva_producto' where id_productos = '$_POST[txt_0]'";								
 				}
 				else{
-					$sql = "update productos set codigo='$_POST[txt_1]',codigo_barras='$_POST[txt_8]',descripcion='".strtoupper($_POST['txt_2'])."',precio='$_POST[txt_9]',utilidad_minorista='$_POST[txt_3]',utilidad_mayorista='$_POST[txt_10]',precio_minorista='$_POST[txt_4]',precio_mayorista='$_POST[txt_11]',id_tipo='$_POST[txt_5]',stock='$_POST[txt_12]',id_categoria='$_POST[txt_6]',id_marca='$_POST[txt_13]',id_bodega='$_POST[txt_7]',id_unidad='$_POST[txt_14]',facturar_existencia='$sin_existencia',cantidad_minima='$_POST[txt_16]',cantidad_maxima='$_POST[txt_17]',id_series_venta='$producto_series',id_lotes='$expiracion_producto',comentario='$_POST[txt_18]',imagen='default.png',estado='$producto_activo',iva_producto='$iva_producto' where id_productos = '$_POST[txt_0]'";					
+					$sql = "update productos set codigo='$_POST[txt_1]',codigo_barras='$_POST[txt_8]',descripcion='".strtoupper($_POST['txt_2'])."',precio='$valor1',utilidad_minorista='$_POST[txt_3]',utilidad_mayorista='$_POST[txt_10]',precio_minorista='$valor2',precio_mayorista='$valor3',id_tipo='$_POST[txt_5]',stock='$_POST[txt_12]',id_categoria='$_POST[txt_6]',id_marca='$_POST[txt_13]',id_bodega='$_POST[txt_7]',id_unidad='$_POST[txt_14]',facturar_existencia='$sin_existencia',cantidad_minima='$_POST[txt_16]',cantidad_maxima='$_POST[txt_17]',id_series_venta='$producto_series',id_lotes='$expiracion_producto',comentario='$_POST[txt_18]',imagen='default.png',estado='$producto_activo',iva_producto='$iva_producto' where id_productos = '$_POST[txt_0]'";					
 				}
 			}	
 			$guardar = guardarSql($conexion,$sql);
