3d15f03f7e110e69b1bef409d271d89870b6c979
deividscriollo <deividscriollo@gmail.com>
2015-02-18 17:42:50 -0500

    proceso pg

---
+++ data/r_estadistico/index.php
@@ -0,0 +1,449 @@
+<?php include('../menu/index.php'); ?>
+<?php
+if(!isset($_SESSION))
+	{
+		session_start();		
+	}
+	if(!isset($_SESSION["iddow"])) {
+
+		header('Location: ../../');
+	}
+?>
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+		<meta charset="utf-8" />
+		<link rel="shortcut icon" href="../../dist/images/logo.fw.png">
+		<title>Inicio - <?php empresa(); ?></title>
+
+		<meta name="description" content="overview &amp; stats" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+
+		<!-- bootstrap & fontawesome -->
+		<link rel="stylesheet" href="../../dist/css/bootstrap.min.css" />
+		<link rel="stylesheet" href="../../dist/css/font-awesome.min.css" />
+		<!-- Select -->
+		<link rel="stylesheet" href="../../dist/css/chosen.min.css" />
+
+
+		<!-- page specific plugin styles -->
+
+		<!-- text fonts -->
+		<link rel="stylesheet" href="../../dist/css/fontdc.css" />
+
+		<!-- ace styles -->
+		<link rel="stylesheet" href="../../dist/css/ace.min.css" class="ace-main-stylesheet" id="main-ace-style" />
+		<link type="text/css" rel="stylesheet" id="ace-skins-stylesheet" href="../../dist/css/ace-skins.min.css">
+
+		<!-- ace settings handler -->
+		<script src="../../dist/js/ace-extra.min.js"></script>
+	</head>
+
+	<body class="skin-1">
+		<?php menu_arriba(); ?>
+
+		<div class="main-container" id="main-container">
+			
+
+			<?php menu_lateral(); ?>
+
+			 <div class="main-content">
+				<div class="main-content-inner">
+					<div class="page-content">
+						<div class="row">
+							<div class="col-xs-12 col-sm-12 widget-container-col">
+								<div class="widget-box">
+									<div class="widget-header">
+										<h5 class="widget-title">Proceso</h5>
+
+										<div class="widget-toolbar">
+											<a href="#" data-action="fullscreen" class="orange2">
+												<i class="ace-icon fa fa-expand"></i>
+											</a>
+
+											<a href="#" data-action="reload">
+												<i class="ace-icon fa fa-refresh"></i>
+											</a>
+										</div>
+									</div>
+
+									<div class="widget-body">
+										<div class="widget-main">											
+										<div class="row">
+											<div class="col-xs-6">												
+												<div class="col-sm-12">
+													<h3 class="row header smaller lighter green">
+														<span class="col-sm-6"> Producto más vendido</span><!-- /.col -->
+
+														<span class="col-sm-6">
+															<label class="pull-right inline">
+																<small class="muted smaller-90"></small>
+
+																<button class="btn btn-app btn-light btn-xs">
+																	<i class="ace-icon fa fa-bar-chart-o bigger-160"></i>
+																	Ver
+																</button>
+															</label>
+														</span><!-- /.col -->
+													</h3>
+												</div>
+												<div class="col-sm-12">
+													<h3 class="row header smaller lighter orange">
+														<span class="col-sm-6"> Cliente que más compra</span><!-- /.col -->
+
+														<span class="col-sm-6">
+															<label class="pull-right inline">
+																<small class="muted smaller-90"></small>
+
+																<button class="btn btn-app btn-light btn-xs">
+																	<i class="ace-icon fa fa-bar-chart-o bigger-160"></i>
+																	Ver
+																</button>
+															</label>
+														</span><!-- /.col -->
+													</h3>
+												</div>
+											</div>
+											<div class="col-xs-6">
+												<div id="piechart-placeholder" style="width: 90%; min-height: 150px; padding: 0px; position: relative;">
+													<canvas class="flot-base" width="411" height="150" style="direction: ltr; position: absolute; left: 0px; top: 0px; width: 411px; height: 150px;">
+														
+													</canvas>
+													<canvas class="flot-overlay" width="411" height="150" style="direction: ltr; position: absolute; left: 0px; top: 0px; width: 411px; height: 150px;">
+														
+													</canvas><div class="legend">
+													<div style="position: absolute; width: 90px; height: 110px; top: 15px; right: -30px; opacity: 0.85; background-color: rgb(255, 255, 255);"> </div>
+													<table style="position:absolute;top:15px;right:-30px;;font-size:smaller;color:#545454">
+														<tbody>
+															<tr>	
+																<td class="legendColorBox">
+																	<div style="border:1px solid null;padding:1px">
+																		<div style="width:4px;height:0;border:5px solid #68BC31;overflow:hidden">																		
+																		</div>
+																	</div>
+																</td>
+																<td class="legendLabel">Clientes</td>
+															</tr>
+															<tr>
+																<td class="legendColorBox">
+																	<div style="border:1px solid null;padding:1px">
+																		<div style="width:4px;height:0;border:5px solid #2091CF;overflow:hidden"></div>
+																	</div>
+																</td>
+																<td class="legendLabel">Provedores</td>
+															</tr>
+															<tr>
+																<td class="legendColorBox">
+																	<div style="border:1px solid null;padding:1px">
+																		<div style="width:4px;height:0;border:5px solid #AF4E96;overflow:hidden"></div>
+																	</div>
+																</td>
+																<td class="legendLabel">Gestion</td>
+															</tr>
+															<tr>
+																<td class="legendColorBox">
+																	<div style="border:1px solid null;padding:1px">
+																		<div style="width:4px;height:0;border:5px solid #DA5430;overflow:hidden"></div>
+																	</div>
+																</td>
+																<td class="legendLabel">Actividades</td>
+															</tr>
+															<tr>
+																<td class="legendColorBox">
+																	<div style="border:1px solid null;padding:1px">
+																		<div style="width:4px;height:0;border:5px solid #FEE074;overflow:hidden"></div>
+																	</div>
+																</td>
+																<td class="legendLabel">Procesos</td>
+															</tr>
+														</tbody>
+													</table>
+												</div>
+											</div>
+											</div>
+										</div>
+										</div>
+									</div>
+								</div>
+							</div>							
+						</div>
+					</div>
+				</div>
+			</div><!-- /.main-content -->
+
+			<?php footer(); ?>
+
+			<a href="#" id="btn-scroll-up" class="btn-scroll-up btn btn-sm btn-inverse">
+				<i class="ace-icon fa fa-angle-double-up icon-only bigger-110"></i>
+			</a>
+		</div><!-- /.main-container -->
+
+		<!-- basic scripts -->
+
+		
+		<!-- <![endif]-->
+
+		<!--[if IE]>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<![endif]-->
+
+		<!--[if !IE]> -->
+		<script type="text/javascript">
+			window.jQuery || document.write("<script src='../../dist/js/jquery.min.js'>"+"<"+"/script>");
+		</script>
+
+		<!-- <![endif]-->
+
+		<!--[if IE]>
+<script type="text/javascript">
+ window.jQuery || document.write("<script src='../../dist/js/jquery1x.min.js'>"+"<"+"/script>");
+</script>
+<![endif]-->
+		<script type="text/javascript">
+			if('ontouchstart' in document.documentElement) document.write("<script src='../../dist/js/jquery.mobile.custom.min.js'>"+"<"+"/script>");
+		</script>
+		<script src="../../dist/js/bootstrap.min.js"></script>
+
+		<!-- page specific plugin scripts -->
+
+		<!--[if lte IE 8]>
+		  <script src="../../dist/js/excanvas.min.js"></script>
+		<![endif]-->
+		<script src="../../dist/js/jquery-ui.custom.min.js"></script>
+		<script src="../../dist/js/jquery.ui.touch-punch.min.js"></script>
+		<script src="../../dist/js/jquery.easypiechart.min.js"></script>
+		<script src="../../dist/js/jquery.sparkline.min.js"></script>
+		<script src="../../dist/js/flot/jquery.flot.min.js"></script>
+		<script src="../../dist/js/flot/jquery.flot.pie.min.js"></script>
+		<script src="../../dist/js/flot/jquery.flot.resize.min.js"></script>
+		<script src="../../dist/js/chosen.jquery.min.js"></script>
+		
+
+
+
+		<!-- ace scripts -->
+		<script src="../../dist/js/ace-elements.min.js"></script>
+		<script src="../../dist/js/ace.min.js"></script>
+
+		<!-- inline scripts related to this page -->
+		<script type="text/javascript">
+			jQuery(function($) {
+				$('.easy-pie-chart.percentage').each(function(){
+					var $box = $(this).closest('.infobox');
+					var barColor = $(this).data('color') || (!$box.hasClass('infobox-dark') ? $box.css('color') : 'rgba(255,255,255,0.95)');
+					var trackColor = barColor == 'rgba(255,255,255,0.95)' ? 'rgba(255,255,255,0.25)' : '#E2E2E2';
+					var size = parseInt($(this).data('size')) || 50;
+					$(this).easyPieChart({
+						barColor: barColor,
+						trackColor: trackColor,
+						scaleColor: false,
+						lineCap: 'butt',
+						lineWidth: parseInt(size/10),
+						animate: /msie\s*(8|7|6)/.test(navigator.userAgent.toLowerCase()) ? false : 1000,
+						size: size
+					});
+				})
+			
+				$('.sparkline').each(function(){
+					var $box = $(this).closest('.infobox');
+					var barColor = !$box.hasClass('infobox-dark') ? $box.css('color') : '#FFF';
+					$(this).sparkline('html',
+									 {
+										tagValuesAttribute:'data-values',
+										type: 'bar',
+										barColor: barColor ,
+										chartRangeMin:$(this).data('min') || 0
+									 });
+				});
+			
+			
+			  //flot chart resize plugin, somehow manipulates default browser resize event to optimize it!
+			  //but sometimes it brings up errors with normal resize event handlers
+			  $.resize.throttleWindow = false;
+			
+			  var placeholder = $('#piechart-placeholder').css({'width':'90%' , 'min-height':'150px'});
+			  var data = [
+				{ label: "social networks",  data: 38.7, color: "#68BC31"},
+				{ label: "search engines",  data: 24.5, color: "#2091CF"},
+				{ label: "ad campaigns",  data: 8.2, color: "#AF4E96"},
+				{ label: "direct traffic",  data: 18.6, color: "#DA5430"},
+				{ label: "other",  data: 10, color: "#FEE074"}
+			  ]
+			  function drawPieChart(placeholder, data, position) {
+			 	  $.plot(placeholder, data, {
+					series: {
+						pie: {
+							show: true,
+							tilt:0.8,
+							highlight: {
+								opacity: 0.25
+							},
+							stroke: {
+								color: '#fff',
+								width: 2
+							},
+							startAngle: 2
+						}
+					},
+					legend: {
+						show: true,
+						position: position || "ne", 
+						labelBoxBorderColor: null,
+						margin:[-30,15]
+					}
+					,
+					grid: {
+						hoverable: true,
+						clickable: true
+					}
+				 })
+			 }
+			 drawPieChart(placeholder, data);
+			
+			 /**
+			 we saved the drawing function and the data to redraw with different position later when switching to RTL mode dynamically
+			 so that's not needed actually.
+			 */
+			 placeholder.data('chart', data);
+			 placeholder.data('draw', drawPieChart);
+			
+			
+			  //pie chart tooltip example
+			  var $tooltip = $("<div class='tooltip top in'><div class='tooltip-inner'></div></div>").hide().appendTo('body');
+			  var previousPoint = null;
+			
+			  placeholder.on('plothover', function (event, pos, item) {
+				if(item) {
+					if (previousPoint != item.seriesIndex) {
+						previousPoint = item.seriesIndex;
+						var tip = item.series['label'] + " : " + item.series['percent']+'%';
+						$tooltip.show().children(0).text(tip);
+					}
+					$tooltip.css({top:pos.pageY + 10, left:pos.pageX + 10});
+				} else {
+					$tooltip.hide();
+					previousPoint = null;
+				}
+				
+			 });
+			
+				/////////////////////////////////////
+				$(document).one('ajaxloadstart.page', function(e) {
+					$tooltip.remove();
+				});
+			
+			
+			
+			
+				var d1 = [];
+				for (var i = 0; i < Math.PI * 2; i += 0.5) {
+					d1.push([i, Math.sin(i)]);
+				}
+			
+				var d2 = [];
+				for (var i = 0; i < Math.PI * 2; i += 0.5) {
+					d2.push([i, Math.cos(i)]);
+				}
+			
+				var d3 = [];
+				for (var i = 0; i < Math.PI * 2; i += 0.2) {
+					d3.push([i, Math.tan(i)]);
+				}
+				
+			
+				var sales_charts = $('#sales-charts').css({'width':'100%' , 'height':'220px'});
+				$.plot("#sales-charts", [
+					{ label: "Domains", data: d1 },
+					{ label: "Hosting", data: d2 },
+					{ label: "Services", data: d3 }
+				], {
+					hoverable: true,
+					shadowSize: 0,
+					series: {
+						lines: { show: true },
+						points: { show: true }
+					},
+					xaxis: {
+						tickLength: 0
+					},
+					yaxis: {
+						ticks: 10,
+						min: -2,
+						max: 2,
+						tickDecimals: 3
+					},
+					grid: {
+						backgroundColor: { colors: [ "#fff", "#fff" ] },
+						borderWidth: 1,
+						borderColor:'#555'
+					}
+				});
+			
+			
+				$('#recent-box [data-rel="tooltip"]').tooltip({placement: tooltip_placement});
+				function tooltip_placement(context, source) {
+					var $source = $(source);
+					var $parent = $source.closest('.tab-content')
+					var off1 = $parent.offset();
+					var w1 = $parent.width();
+			
+					var off2 = $source.offset();
+					//var w2 = $source.width();
+			
+					if( parseInt(off2.left) < parseInt(off1.left) + parseInt(w1 / 2) ) return 'right';
+					return 'left';
+				}
+			
+			
+				$('.dialogs,.comments').ace_scroll({
+					size: 300
+			    });
+				
+				
+				//Android's default browser somehow is confused when tapping on label which will lead to dragging the task
+				//so disable dragging when clicking on label
+				var agent = navigator.userAgent.toLowerCase();
+				if("ontouchstart" in document && /applewebkit/.test(agent) && /android/.test(agent))
+				  $('#tasks').on('touchstart', function(e){
+					var li = $(e.target).closest('#tasks li');
+					if(li.length == 0)return;
+					var label = li.find('label.inline').get(0);
+					if(label == e.target || $.contains(label, e.target)) e.stopImmediatePropagation() ;
+				});
+			
+				$('#tasks').sortable({
+					opacity:0.8,
+					revert:true,
+					forceHelperSize:true,
+					placeholder: 'draggable-placeholder',
+					forcePlaceholderSize:true,
+					tolerance:'pointer',
+					stop: function( event, ui ) {
+						//just for Chrome!!!! so that dropdowns on items don't appear below other items after being moved
+						$(ui.item).css('z-index', 'auto');
+					}
+					}
+				);
+				$('#tasks').disableSelection();
+				$('#tasks input:checkbox').removeAttr('checked').on('click', function(){
+					if(this.checked) $(this).closest('li').addClass('selected');
+					else $(this).closest('li').removeClass('selected');
+				});
+			
+			
+				//show the dropdowns on top or bottom depending on window height and menu position
+				$('#task-tab .dropdown-hover').on('mouseenter', function(e) {
+					var offset = $(this).offset();
+			
+					var $w = $(window)
+					if (offset.top > $w.scrollTop() + $w.innerHeight() - 100) 
+						$(this).addClass('dropup');
+					else $(this).removeClass('dropup');
+				});
+			
+			})
+		</script>
+
+	</body>
+</html>
