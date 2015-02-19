72076c7d8ecbcd3d1cf4e2e6a44211e66940aea4
Willy Narvaez <willynarvaez6@gmail.com>
2015-02-19 18:08:14 -0500

    heroku

--- data/conexion.php
+++ data/conexion.php
@@ -4,8 +4,8 @@
     	$conexion = null;
         try
         {                             
-        //$conexion = pg_connect("dbname=ddr8bislb4p21h host=ec2-54-225-243-113.compute-1.amazonaws.com port=5432 user=hmadfgkigcyfly password=tx24DCRCiJBKcMpEUtJjqJ0VnD sslmode=require");
-        $conexion = pg_connect("host=localhost dbname=aplicacion_dow port=5432 user=postgres password=rootdow");
+        $conexion = pg_connect("dbname=ddr8bislb4p21h host=ec2-54-225-243-113.compute-1.amazonaws.com port=5432 user=hmadfgkigcyfly password=tx24DCRCiJBKcMpEUtJjqJ0VnD sslmode=require");
+        //$conexion = pg_connect("host=localhost dbname=aplicacion_dow port=5432 user=postgres password=rootdow");
          if( $conexion == false )
                  throw new Exception( "Error PostgreSQL ".pg_last_error() );
         }
